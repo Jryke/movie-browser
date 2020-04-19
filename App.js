0fcd7a6da23e721e8dc07e0b8a5f792a07fb7c20
Jryke <rykerson@gmail.com>
2020-04-18 21:13:45 -0400

    react-navigation/stack installed, App component refactored to navigate between child components

--- App.js
+++ App.js
@@ -2,8 +2,12 @@ import React, { useState, useEffect } from 'react';
 import { StyleSheet, View } from 'react-native';
 import 'react-native-gesture-handler';
 import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 import { search } from './mockData';
 import Home from './screens/Home';
+import MovieInfo from './screens/MovieInfo';
+
+const Stack = createStackNavigator();
 
 export default function App() {
   const [inputVal, setInputVal] = useState('');
@@ -16,18 +20,14 @@ export default function App() {
 
   return (
     <NavigationContainer>
-      <View style={styles.container}>
-      <Home inputVal={inputVal} setInputVal={setInputVal} movieList={movieList} setSelectedMovie={setSelectedMovie} />
-      </View>
+      <Stack.Navigator initialRouteName='Home'>
+        <Stack.Screen name='Home'>
+          {props => <Home {...props} inputVal={inputVal} setInputVal={setInputVal} movieList={movieList} setSelectedMovie={setSelectedMovie} />}
+        </Stack.Screen>
+        <Stack.Screen name='Info'>
+          {props => <MovieInfo {...props} movie={selectedMovie} />}
+        </Stack.Screen>
+      </Stack.Navigator>
     </NavigationContainer>
   );
 };
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
