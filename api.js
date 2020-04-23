1ee3813f468cb36b5c8afd5d9630972ad81b28d4
Jryke <rykerson@gmail.com>
2020-04-23 15:26:39 -0400

    fetch added for individual movie, deserialize function added for use in both fetches

--- api.js
+++ api.js
@@ -1,5 +1,23 @@
+const BASE_URL = 'http://www.omdbapi.com/?apikey=744db6a4';
+
+const deserializeResponse = response => {
+  if (!response.ok) throw new Error('Error fetching your request')
+
+  return response.json()
+}
+
 export const fetchMovies = async (input) => {
-  const response = await (await fetch(`http://www.omdbapi.com/?apikey=744db6a4&r=json&s=${input}`)).json()
-  console.log(response);
-  return response.Search
+  const searchURL = `${BASE_URL}&r=json&s=${input}`
+  const response = await fetch(searchURL)
+  const result = await deserializeResponse(response)
+  console.log('result:', result)
+  return result.Search
+}
+
+export const fetchMovieInfo = async (id) => {
+  const searchURL = `${BASE_URL}&i=${id}`
+  const response = await fetch(searchURL)
+  const result = await deserializeResponse(response)
+  console.log('result:', result)
+  return result
 }
