da083ca57d86e4a80ba517c323d0efe3504d38e2
Jryke <rykerson@gmail.com>
2020-04-23 15:31:30 -0400

    movie details fetching and used to render data with basic styling

--- screens/MovieInfo.js
+++ screens/MovieInfo.js
@@ -1,18 +1,49 @@
-import React from 'react';
-import { StyleSheet, View, Text, Image } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { StyleSheet, View, Text, Image, ActivityIndicator } from 'react-native';
+import PropTypes from 'prop-types';
+import { fetchMovieInfo } from '../api';
+
+const MovieLoadingScreen = () => {
+  return (
+    <View style={styles.loadingContainer}>
+      <ActivityIndicator size="large" color="black" />
+    </View>
+  )
+}
 
 const MovieInfo = ({ route }) => {
-  const { movie } = route.params;
+  const movieId = route.params.movie.imdbID;
+  const [movieDetails, setMovieDetails] = useState();
+
+  useEffect(() => {
+    fetchMovieInfo(movieId).then(res => setMovieDetails(res))
+  }, [movieId])
+
+  if (!movieDetails) return <MovieLoadingScreen />
   return (
     <View style={styles.container}>
-      <Image style={styles.poster} source={movie.Poster} />
-      <Text style={styles.title}>{movie.Title}</Text>
-      <Text>({movie.Year})</Text>
+      <Image style={styles.poster} source={{url: movieDetails.Poster}} />
+      <View style={styles.row}>
+        <Text style={styles.title}>{movieDetails.Title}</Text>
+        <Text>({movieDetails.Year})</Text>
+      </View>
+      <View style={styles.row}>
+        <Text>{movieDetails.Plot}</Text>
+      </View>
+      {
+        movieDetails.Ratings.map((rating, i) => (
+          <Text style={styles.row} key={i}>{rating.Source}: {rating.Value}</Text>
+        ))
+      }
     </View>
   );
 };
 
 const styles = StyleSheet.create({
+  loadingContainer: {
+    flex: 1,
+    justifyContent: 'center',
+  },
   container: {
     flex: 1,
     backgroundColor: '#fff',
@@ -21,12 +52,21 @@ const styles = StyleSheet.create({
   },
   poster: {
     width: 300,
-    height: 300,
+    height: 450,
+  },
+  row: {
+    alignItems: 'center',
+    paddingVertical: 10,
+    paddingHorizontal: 30,
   },
   title: {
     fontSize: 20,
     fontWeight: '700',
   }
 });
+
+MovieInfo.propTypes = {
+  route: PropTypes.object
+}
 
 export default MovieInfo;
